8d8580ecc3a00b4bc0f7c9567d7b6637318c7e8f
Maciej Mika <maciej.mika@synetic.pl>
2023-08-10 22:04:42 +0200

    Add copy.sh

---
+++ scripts/copy.sh
@@ -0,0 +1,3 @@
+#rsync -avr --exclude "*venv*" --exclude ".git*" --exclude ".idea"  ../ pi@192.168.68.102:/home/pi/my-ufh-relay-controller
+rsync -avr --exclude "*venv*" --exclude ".git*" --exclude ".idea"  ../ pi@192.168.68.106:/home/pi/my-ufh-relay-controller
+#rsync -avr --exclude "*venv*" --exclude ".git*" --exclude ".idea"  ../ pi@192.168.68.108:/home/pi/my-ufh-relay-controller
